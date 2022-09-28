3ec79a6e7902088b8afb6af4d2cf0fae2b37550f
Lars Kecker <lars.kecker2009@gmail.com>
2022-09-28 20:26:47 +0200

    fix tests

--- src/main/kotlin/de/spotifyvotingqueue/svqbackend/config/SpotifyApiConfig.kt
+++ src/main/kotlin/de/spotifyvotingqueue/svqbackend/config/SpotifyApiConfig.kt
@@ -3,23 +3,22 @@ package de.spotifyvotingqueue.svqbackend.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.stereotype.Component
 import se.michaelthelin.spotify.SpotifyApi
 
 @Configuration
 class SpotifyApiConfig {
 
-    @Value("\${spring.security.oauth2.client.registration.spotify.client_id}")
-    private val client_id: String? = null;
+    @Value("\${spring.security.oauth2.client.registration.spotify.client-id}")
+    private val clientId: String? = null;
 
-    @Value("\${spring.security.oauth2.client.registration.spotify.client_secret}")
-    private val client_secret: String? = null;
+    @Value("\${spring.security.oauth2.client.registration.spotify.client-secret}")
+    private val clientSecret: String? = null;
 
     @Bean
-    public fun getSpotifyApi() : SpotifyApi {
+    fun getSpotifyApi() : SpotifyApi {
         return SpotifyApi.Builder()
-            .setClientId(client_id)
-            .setClientSecret(client_secret)
+            .setClientId(clientId)
+            .setClientSecret(clientSecret)
             .build();
     }
 }
